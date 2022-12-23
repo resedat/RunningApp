cc47d71ee11157f799fd76291c6e2bcd9fd8d370
Sedat Barlin <barlinsedat@gmail.com>
2022-12-23 03:19:33 +0300

    Running App

---
+++ KosuApp/KosuApp/Model/Oneri.swift
@@ -0,0 +1,14 @@
+//
+//  Oneri.swift
+//  KosuApp
+//
+//  Created by Sedat on 14.12.2022.
+//
+
+import Foundation
+
+struct Oneri{
+    var adi: String
+    var detaylari: String
+    var goruntuAdi: String
+}
