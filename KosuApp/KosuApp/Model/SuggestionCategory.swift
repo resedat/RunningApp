926dc23c4579de7906361c36acc6813571d7034f
Sedat Barlin <barlinsedat@gmail.com>
2022-12-27 00:51:08 +0300

    Running App

---
+++ KosuApp/KosuApp/Model/SuggestionCategory.swift
@@ -0,0 +1,13 @@
+//
+//  SuggestionCategory.swift
+//  KosuApp
+//
+//  Created by Sedat on 14.12.2022.
+//
+
+import Foundation
+
+struct SuggestionCategory {
+    let ka: String
+    let viewName: String
+}
