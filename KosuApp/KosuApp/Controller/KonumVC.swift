28a7a9eae96d0aec9c702d0ce37fc130f2f31879
Sedat Barlin <barlinsedat@gmail.com>
2022-12-26 16:50:33 +0300

    Running App

---
+++ KosuApp/KosuApp/Controller/KonumVC.swift
@@ -0,0 +1,33 @@
+//
+//  KonumViewController.swift
+//  KosuApp
+//
+//  Created by Sedat on 21.11.2022.
+//
+
+import UIKit
+import MapKit
+
+class KonumVC: UIViewController, MKMapViewDelegate {
+    
+    var manager : CLLocationManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+    }
+    
+    func izinKontrol(){
+        
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager?.requestWhenInUseAuthorization()
+        }
+        
+    }
+    
+    
+
+}
