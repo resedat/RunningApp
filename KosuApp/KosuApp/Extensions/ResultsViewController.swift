1d8568fbbf6af6e9ac971de4c043974eadc2f971
Sedat Barlin <barlinsedat@gmail.com>
2023-01-03 14:01:09 +0300

    Running App

---
+++ KosuApp/KosuApp/Extensions/ResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultsViewController.swift
+//  KosuApp
+//
+//  Created by Sedat on 30.11.2022.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
