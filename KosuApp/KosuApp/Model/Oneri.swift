7b78c55778ef7601c6e9ece2ee83a58997d125c2
Sedat Barlin <barlinsedat@gmail.com>
2023-01-10 23:47:21 +0300

    run

---
+++ KosuApp/KosuApp/Model/Oneri.swift
@@ -0,0 +1,14 @@
+//
+//  Oneri.swift
+//  KosuApp
+//
+//  Created by Sedat on 14.12.2022.
+//
+
+import Foundation
+
+struct Oneri{
+    let adi: String
+    let detaylari: String
+    let goruntuAdi: String
+}
---
+++ KosuApp/KosuApp/Model/Sabitler.swift
@@ -0,0 +1,10 @@
+//
+//  Sabitler.swift
+//  KosuApp
+//
+//  Created by Sedat on 22.11.2022.
+//
+
+import Foundation
+
+let REALM_KUYRUK = DispatchQueue(label: "realmKuyruk")
